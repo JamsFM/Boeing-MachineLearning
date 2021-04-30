a9e87e49d38c38eea7cbec14d11d8e5d0588a053
Travis23s <77144033+Travis23s@users.noreply.github.com>
2021-04-30 15:49:42 -0700

    Update SVM.py

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -3,6 +3,7 @@ import os
 import numpy as np #TODO: Check if these libraries are needed
 import pandas as pd
 import pickle
+import re
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 import random
@@ -78,7 +79,6 @@ def MakePredictions(): # Writes each log and its corresponding prediction to a f
     predictions = loadedModel.predict(testingLogs)
     result = loadedModel.score(testingLogs, testingLabels)
     print("Loaded model accuracy: " + str(result))
-    print(predictions)
 
 
     #write predictions to output file
@@ -96,8 +96,68 @@ def MakePredictions(): # Writes each log and its corresponding prediction to a f
         f.write(f'{frequencies.get(predictions[i]):.4g}')
         f.write("%   log: ")
         f.write(logs[i]) # Write original log to file (not inluding '\n')
-        #f.write("\n")
+        f.write(featureExtract(logs[i]))
+        f.write("\tLine Number: " + str(i+1) + "\n\n")
     f.close()
+
+
+def featureExtract(line):
+    features = ""
+    if re.search("^\[",line): #log starts with a square bracket
+       # print("This is a error.log log file")
+        line = re.split("\[", line, 1)[1]
+        date = re.split("(?<=.{10})\s", line, 1)[0] #gets first part of date. Will be concatenated with year later
+        line = re.split("(?<=.{10})\s", line, 1)[1]
+        time = re.split("\s", line, 1)[0]
+        line = re.split("\s", line, 1)[1]
+        date = date + " " + re.split("]", line, 1)[0] #concatenates year with the rest of the date
+        line = re.split("]", line, 1)[1]
+        line = re.split("\[", line, 1) [1]
+        logType = re.split("\]", line, 1) [0]
+        line = re.split("\[", line, 1) [1]
+        IDs = re.split("\]", line, 1) [0]
+        PID = re.split("\s", IDs, 1)[1]
+        PID = re.split("\:", PID, 1)[0]
+        TID = re.split("\s", IDs, 2)[2]
+        client = ""
+        if re.search("\[(?=client)", line): #ignore if brackets arent for client
+            line = re.split("\[", line, 1)[1]
+            line = re.split("\s", line, 1)[1]
+            client = re.split("\]", line, 1)[0]
+        line = re.split("\s(?=A)", line, 1)[1] #find space followed by "A" 
+        errorCode = re.split("\:", line, 1)[0]
+        msg = re.split("\:", line, 1)[1]
+        features = "\ttime = " + time + "\n\t" + "date = " + date + "\n\t" + "log type = " + logType + "\n\t" + "PID = " + PID + "\n\t" + "TID = " + TID + "\n\t" + "client = " + client + "\n\t" + "error code = " + errorCode + "\n\t" + "message =" + msg
+        
+        
+    elif re.search("^\d",line): #log starts with a digit
+        #print("This is a access.log log file") #used for malicious web server access bad actor
+        #log format:
+        #ip - user (- if not relevant) [date & time] "GET \X"-"(if not needed)
+            #else "GET \X" \d \d "http://ip/" "Browser information"
+        ip = re.split("\s", line, 1)[0]
+        line = re.split("\s", line, 1)[1]
+        line = re.split("(?<=-)\s", line, 1)[1]
+        user = re.split("\s", line, 1)[0]
+        line = re.split("\s\[", line, 1)[1]
+        date = re.split(":", line, 1)[0]
+        line = re.split(":", line, 1)[1]
+        time = re.split("]", line, 1)[0]
+        msg = re.split("] ", line, 1)[1]
+        features = "\tip = " + ip + "\n\t" + "user = " + user + "\n\t" + "date = " + date + "\n\t" + "time = " + time + "\n\t" + "message = " + msg
+
+    elif re.search("^[A-Za-z]",line): #log starts with a alphabetical character
+        #print("This is a auth.log log file")
+        #log format:
+        #month  day hour:minute:second user useraccount logtype: main information
+        date = re.split("(?<=.{6})\s", line, 1)[0]
+        line = re.split("(?<=.{6})\s", line, 1)[1]
+        time = re.split("(?<=.{8})\s", line, 1)[0]
+        line = re.split("(?<=.{8})\s", line, 1)[1]
+        user = re.split("\s", line)[0]
+        msg = re.split("\s", line, 1)[1]
+        features = "\tdate = " + date + "\n\t" + "time = " + time + "\n\t" + "user = " + user + "\n\t" + "message = " + msg
+    return features
 
 ProcessLogs()
 ModelTrain()
