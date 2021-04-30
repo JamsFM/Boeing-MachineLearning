863c88d255ab2aa9196044348d2f41ff2ee3d8f1
Travis23s <77144033+Travis23s@users.noreply.github.com>
2021-04-29 18:09:58 -0700

    Update SVM.py

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -14,19 +14,14 @@ def ProcessLogs(): # Makes each log into an array with the first element being t
     """ #TODO: This is the old method to import logs and labels. May remove if unneeded, but I'm leaving this for now incase it is helpful later.
     # Vectorizes log file into numerical array
     logs = open(os.path.join(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))), 'universalSet.txt'), 'r', encoding="utf8") #Opens logs.txt (has to be in same directory as python script)
-
     # Training labels for multinomial universal set
     trainingLabels = [0, 2, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 2, 3, 0, 0, 0, 0, 0, 0, 0, 5, 0, 5, 0, 0, 4, 4, 0, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 6, 0, 0, 0, 1, 5, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 6, 1, 0, 0, 0, 2, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 5, 0, 0, 0, 6, 1, 6, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 6, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 2, 6, 6, 2, 0, 0, 0, 0, 0, 0, 4, 0, 0, 4, 0, 0, 0, 0, 0, 0, 2, 6, 0, 3, 3, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 2, 3, 0, 0, 0, 0, 0, 5, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 4, 0, 0, 5, 6, 6, 3, 0, 0, 4, 0, 0, 6, 0, 0, 0, 0, 1, 0, 0, 0, 5, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0, 0, 0, 0, 0, 6, 0, 0, 4, 0, 5, 0, 0, 0, 0, 0, 0, 5, 0, 6, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 2, 0, 0, 6, 0, 0, 0, 0, 6, 5, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 5, 0, 0, 5, 0, 3, 4, 0, 5, 0, 0, 0, 4, 0, 0, 0, 3, 0, 0, 2, 0, 0, 0, 5, 2, 1, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 4, 0, 0, 0, 4, 0, 0, 3, 0, 0, 3, 0, 0, 4, 0, 6, 0, 0, 0, 0, 5, 3, 0, 4, 0, 0, 0, 0, 0, 4, 0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 4, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 6, 0, 0, 0, 2, 0, 3, 0, 0, 0, 0, 2, 3, 0, 0, 1, 1, 6, 0, 2, 0, 0, 6, 0, 0, 6, 1, 0, 0, 0, 6, 0, 0, 0, 0, 0, 1, 0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 0, 5, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 6, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 4, 5, 0, 0, 0, 0, 0, 0, 1, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 4, 0, 0, 0, 0, 0, 2, 0, 6, 0, 2, 0, 3, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 0, 3, 1, 0, 0, 0, 0, 0, 5, 6, 0, 1, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 0, 2, 6, 0, 0, 2, 0, 4, 0, 0, 0, 5, 0, 0, 0, 1, 0, 1, 0, 3, 0, 0, 0, 1, 3, 5, 0, 6, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 3, 0, 0, 2, 0, 0, 0, 0, 2, 0, 5, 0, 6, 4, 0, 0, 0, 0, 5, 0, 0, 5, 0, 1, 0, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 4, 4, 1, 0, 0, 0, 0, 2, 5, 0, 0, 4, 2, 0, 0, 0, 1, 0, 0, 5, 0, 0, 1, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 3, 0, 1, 0, 6, 5, 6, 0, 6, 0, 0, 5, 0, 0, 3, 0, 4, 0, 3, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 6, 0, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0]
-
     # Training labels for binomial universal set
     #trainingLabels = [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-
     # Training labels for bad actor logs@@@@@@
     #trainingLabels = ["Unauthorized Web Server Logins","Unauthorized Web Server Logins","Unauthorized Web Server Logins","Unauthorized Web Server Logins","Unauthorized Web Server Logins","Unauthorized Web Server Logins","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","malicious webserver access","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","ddos","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","port scan","unauthorized login","unauthorized login","unauthorized login","unauthorized login","unauthorized login","unauthorized login","unauthorized login","unauthorized login","unauthorized login","unauthorized superuser privileges","unauthorized superuser privileges","unauthorized superuser privileges","unauthorized superuser privileges","failed login","failed login","failed login","unauthorized login","unauthorized login","unauthorized login"]
-
     #for i in trainingLabels
     #    if trainingLabels[i]
-
     vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform(logs)
     featureNames = vectorizer.get_feature_names()
@@ -43,82 +38,70 @@ def ProcessLogs(): # Makes each log into an array with the first element being t
 
 
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    logFilename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    fileNames = []
+    for i in range(5): #in order: trainingSet,testingSet,trainingLabels,testingLabels,vectorizerModel
+        fileNames.append(askopenfilename())
 
-    global logSet
-    logSet = open(logFilename, 'r', encoding='utf8') #Opens logs.txt (has to be in same directory as python script)
+    global trainingLogs, testingLogs, trainingLabels, testingLabels
+    with open(fileNames[4], 'rb') as f:
+        vectorizer = pickle.load(f)
 
-    # Fits TfidfVectorizer with logs
-    vectorizer = TfidfVectorizer()
-    vectorizer.fit(logSet)
-    print("Vectorizer Trained.")
+    with open(fileNames[0], 'r', encoding='utf8') as f:
+        vectors = vectorizer.transform(f)
+        dense = vectors.todense()
+        trainingLogs = np.asarray(dense)
+
+    with open(fileNames[1], 'r', encoding='utf8') as f:
+        vectors = vectorizer.transform(f)
+        dense = vectors.todense()
+        testingLogs = np.asarray(dense)
+
+    with open(fileNames[2], 'r', encoding='utf8') as f:
+        trainingLabels = json.load(f)
+
+
+    with open(fileNames[3], 'r', encoding='utf8') as f:
+        testingLabels = json.load(f)
+
+    labelTranslation = ["safe", "ssh", "ws", "sql", "ddos", "ps", "su"]
+    trainingLabelsString = []
+    testingLabelsString = []
+    for i in range(len(trainingLabels)):
+        label = trainingLabels[i]
+        trainingLabels[i] = labelTranslation[label]
+    for i in range(len(testingLabels)):
+        label = testingLabels[i]
+        testingLabels[i] = labelTranslation[label]
+    
 
     # Returns position in file to original file
-    logSet.seek(0)
+    testingSet = open(fileNames[1], 'r', encoding="utf8")
     global logs 
     logs = []
-    for message in logSet:
+    for message in testingSet:
         logs.append(message)
-    logSet.seek(0)
-
-    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    labelsFilename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-
-    with open(labelsFilename, 'r', encoding='utf8') as file:
-        logLabels = np.array(json.load(file))
-
-    logLabelStrings = []
-    labelTranslation = ["safe", "ssh", "ws", "sql", "ddos", "ps", "su"]
-    for i in range(len(logLabels)):
-        logLabelStrings.append(labelTranslation[logLabels[i]])
-    # Transforms logs into tf-idf matrix
-    # and outputs as array
-    vectors = vectorizer.transform(logSet)
-    dense = vectors.todense()
-    logSet = np.asarray(dense)
-    
-    # Printing the dataset shape 
-    #print ("Dataset Length: ", len(processedLogs)) 
-    #print ("Dataset Shape: ", processedLogs.shape)
-    # Printing the dataset obseravtions 
-    #print ("Dataset: ",processedLogs.head())
-    #print(feature_names)
-    #print(df)
-    return logSet, logLabelStrings
-
-from sklearn.model_selection import train_test_split 
-# Function to split the dataset 
-def SplitDataset():
-    processedLogs = ProcessLogs()
-    # Separating the target variable 
-    tfidfLogs = processedLogs[0]
-    Labels = processedLogs[1]
-
-    # Splitting the dataset into train and test 
-    xTrain, xTest, yTrain, yTest = train_test_split(tfidfLogs, Labels, test_size = 0.2) 
-    
-    return tfidfLogs, Labels, xTrain, xTest, yTrain, yTest 
 
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 def ModelTrain(): # Train and save a support vector machines (SVM) algorithm
-    tfidfLogs, Labels, xTrain, xTest, yTrain, yTest = SplitDataset()
+    
 
     clf = SVC(kernel='rbf') # Uses a radial basis function kernel
-    clf.fit(xTrain,yTrain)
-    yPred = clf.predict(xTest)
-    print("Accuracy score of trained sklearn model: " + str(accuracy_score(yTest,yPred)))
+    clf.fit(trainingLogs, trainingLabels)
+    predictions = clf.predict(trainingLogs)
+    print("Accuracy score of trained sklearn model: " + str(accuracy_score(trainingLabels,predictions)))
     pickle.dump(clf, open('SVMModel.sav', 'wb')) # Save the model
 
 def MakePredictions(): # Writes each log and its corresponding prediction to a file called "AlgorithmOutput.txt"
     # Loads model, and makes prediction
     loadedModel = pickle.load(open('SVMModel.sav', 'rb'))
-    tfidfLogs, Labels, xTrain, xTest, yTrain, yTest = SplitDataset()
-    predictions = loadedModel.predict(tfidfLogs)
-    result = loadedModel.score(tfidfLogs, Labels)
+    predictions = loadedModel.predict(testingLogs)
+    result = loadedModel.score(testingLogs, testingLabels)
     print("Loaded model accuracy: " + str(result))
+    print(predictions)
 
+    """
     #write predictions to output file
     f = open("AlgorithmOutput.txt", "w") #"w" will overwrite any existing content, "a" will append to the end of the file. Will make a file called "AlgorithmOutput.txt" if one doesn't already exist
     frequencies = {'safe': 0, 'ws': 0, 'sql': 0, 'ddos': 0, 'ps': 0, 'ssh': 0, 'su': 0}
@@ -136,6 +119,7 @@ def MakePredictions(): # Writes each log and its corresponding prediction to a f
         f.write(logs[i]) # Write original log to file (not inluding '\n')
         #f.write("\n")
     f.close()
-
+"""
+ProcessLogs()
 ModelTrain()
-MakePredictions()
+MakePredictions() 
